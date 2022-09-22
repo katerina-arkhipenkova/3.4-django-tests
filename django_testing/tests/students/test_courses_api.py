1039fde692275b118dcf8c6c60a66676e3ba6937
katerina-arkhipenkova <katerina.arkhipenkova@yandex.ru>
2022-09-22 21:51:56 +0300

    first message

---
+++ django_testing/tests/students/test_courses_api.py
@@ -0,0 +1,100 @@
+import pytest
+from rest_framework.test import APIClient
+from model_bakery import baker
+
+from students.models import Student, Course
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+
+    return factory
+
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs, make_m2m=True)
+
+    return factory
+
+
+@pytest.mark.django_db
+def test_get_one_course(client, course_factory):
+    courses = course_factory(_quantity=1)
+    response = client.get('/api/v1/courses/')
+    id = response.data[0]['id']
+    name = response.data[0]['name']
+    response = client.get(f'/api/v1/courses/{id}/')
+    assert response.status_code == 200
+    assert response.data['name'] == name
+
+
+@pytest.mark.django_db
+def test_get_course_list(client, course_factory):
+    courses = course_factory(_quantity=10)
+    response = client.get('/api/v1/courses/')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) == 10
+
+
+@pytest.mark.django_db
+def test_filter_course_by_id(client, course_factory):
+    courses = course_factory(_quantity=10)
+    response = client.get('/api/v1/courses/')
+    id = response.data[5]['id']
+    response = client.get(f'/api/v1/courses/{id}/')
+    assert response.status_code == 200
+    assert response.data['id'] == id
+
+
+@pytest.mark.django_db
+def test_filter_course_by_name(client, course_factory):
+    courses = course_factory(_quantity=10)
+    response = client.get('/api/v1/courses/')
+    name = response.data[5]['name']
+    response = client.get(f'/api/v1/courses/?name={name}')
+    assert response.status_code == 200
+    assert response.data[0]['name'] == name
+
+
+@pytest.mark.django_db
+def test_create_course(client):
+    course = {'id': 1, 'name': 'test'}
+    # Course.objects.create(id=1, name='test')
+    # response = client.get('/api/v1/courses/')
+    response = client.post('/api/v1/courses/', data=course)
+    assert response.status_code == 201
+    assert response.data['name'] == 'test'
+
+
+@pytest.mark.django_db
+def test_update_course(client, course_factory):
+    courses = course_factory(_quantity=10)
+    updated_course = {'name': 'test'}
+    response = client.patch(f'/api/v1/courses/5/', data=updated_course)
+    response = client.get('/api/v1/courses/')
+    assert response.status_code == 200
+    assert response.data[4]['name'] == 'test'
+
+
+@pytest.mark.django_db
+def test_delete_course(client, course_factory):
+    courses = course_factory(_quantity=10)
+    response = client.get('/api/v1/courses/')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) == 10
+    response = client.delete('/api/v1/courses/5/')
+    response = client.get('/api/v1/courses/')
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) == 9
