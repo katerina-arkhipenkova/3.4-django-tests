87681ce497f38a3aa6cab9be2911d36e1a89bdf0
katerina-arkhipenkova <katerina.arkhipenkova@yandex.ru>
2022-09-23 21:56:14 +0300

    filter id changed

--- django_testing/tests/students/test_courses_api.py
+++ django_testing/tests/students/test_courses_api.py
@@ -51,9 +51,9 @@ def test_filter_course_by_id(client, course_factory):
     courses = course_factory(_quantity=10)
     response = client.get('/api/v1/courses/')
     id = response.data[5]['id']
-    response = client.get(f'/api/v1/courses/{id}/')
+    response = client.get(f'/api/v1/courses/?id={id}')
     assert response.status_code == 200
-    assert response.data['id'] == id
+    assert response.data[0]['id'] == id
 
 
 @pytest.mark.django_db
@@ -69,8 +69,6 @@ def test_filter_course_by_name(client, course_factory):
 @pytest.mark.django_db
 def test_create_course(client):
     course = {'id': 1, 'name': 'test'}
-    # Course.objects.create(id=1, name='test')
-    # response = client.get('/api/v1/courses/')
     response = client.post('/api/v1/courses/', data=course)
     assert response.status_code == 201
     assert response.data['name'] == 'test'
@@ -80,7 +78,9 @@ def test_create_course(client):
 def test_update_course(client, course_factory):
     courses = course_factory(_quantity=10)
     updated_course = {'name': 'test'}
-    response = client.patch(f'/api/v1/courses/5/', data=updated_course)
+    response = client.get('/api/v1/courses/')
+    id = response.data[4]['id']
+    response = client.patch(f'/api/v1/courses/{id}/', data=updated_course)
     response = client.get('/api/v1/courses/')
     assert response.status_code == 200
     assert response.data[4]['name'] == 'test'
@@ -93,7 +93,8 @@ def test_delete_course(client, course_factory):
     data = response.json()
     assert response.status_code == 200
     assert len(data) == 10
-    response = client.delete('/api/v1/courses/5/')
+    id = response.data[4]['id']
+    response = client.delete(f'/api/v1/courses/{id}/')
     response = client.get('/api/v1/courses/')
     data = response.json()
     assert response.status_code == 200
